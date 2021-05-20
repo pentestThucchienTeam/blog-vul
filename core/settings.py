09258cf89cc5d1619864619a9a55b8d995b40987
wolfasterest <sonaster2k@gmail.com>
2021-05-20 23:12:41 +0700

    Update move sénitive data into env

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 # load production server from .env
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
