6374b47eafcf68cb37ced16e6d39c1d2f37a6a99
DoTuan1 <thanhtuandohalai@gmail.com>
2021-05-21 00:39:45 +0700

    new_model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,50 +1,46 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class User(models.Model):
-    id = models.CharField(max_length=6)
-    username = models.CharField(max_length=6, primary_key=True)
-    password = models.CharField(max_length=15, )
-    email = models.EmailField(max_length=254)
+    username = models.CharField(max_length=6, default='')
+    password = models.CharField(max_length=15, default='')
+    email = models.EmailField(max_length=254, default='')
 
 
-class Tag(models.Model):
-    id = models.CharField(max_length=6)
-    name = models.CharField(max_length=20)
-    tags = models.CharField(max_length=20, primary_key=True)
+class Tags(models.Model):
+    name = models.CharField(max_length=20, default='')
+    tags = models.CharField(max_length=20, default='')
 
 
 class Profile(models.Model):
-    id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    email = models.EmailField(max_length=254)
-    phoneNumber = models.CharField(max_length=10)
-    firtName = models.CharField(max_length=7)
-    lastName = models.CharField(max_length=7)
+    email = ForeignKey(User, on_delete=models.CASCADE, default='')
+    phoneNumber = models.CharField(max_length=10, default='')
+    firtName = models.CharField(max_length=7, default='')
+    lastName = models.CharField(max_length=7, default='')
     avatar = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-    status = models.CharField(max_length=200)
-    facebook = models.CharField(max_length=200)
-    github = models.CharField(max_length=200)
-    twitter = models.CharField(max_length=200)
+    status = models.TextField(default='')
+    facebook = models.CharField(max_length=200, default='')
+    github = models.CharField(max_length=200, default='')
+    twitter = models.CharField(max_length=200, default='')
 
 
 class Post(models.Model):   
-    id = models.CharField(max_length=6, primary_key=True)
-    title = models.CharField(max_length=30)
-    tags = models.OneToOneField(Tag, on_delete=models.CASCADE)
-    status = models.CharField(max_length=50)
-    content = models.TextField
+    title = models.CharField(max_length=30, default='')
+    tags = ForeignKey(Tags, on_delete=models.CASCADE, default='')
+    status = models.TextField(default='')
+    content = models.TextField(default='')
     createTime = models.DateTimeField(auto_now_add=True)
     updateTime = models.DateTimeField(auto_now_add=True)
-    authorid = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = ForeignKey(User, on_delete=models.CASCADE, default='')
     images = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
 
 
 class Comment(models.Model):
-    id = models.CharField(max_length=6, primary_key=True)
-    content = models.TextField
-    status = models.BooleanField
+    content = models.TextField(default='')
+    status = models.TextField(default='')
     creaaTime = models.DateTimeField(auto_now_add=True)
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = ForeignKey(User, on_delete=models.CASCADE, default='', related_name='cmt_user')
     url = models.URLField
-    email = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    post_id = models.OneToOneField(Post, on_delete=models.CASCADE)
+    email = ForeignKey(User, on_delete=models.CASCADE,default='', related_name='cmt_email')
+
