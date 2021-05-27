fa672c49c53788cfeee5c83124b5ad06f4bc5406
tuannh <tuannh@gmail.com>
2021-05-27 21:54:43 +0700

    fix conflict

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# echo "Flush the manage.py command it any"
+#echo "Flush the manage.py command it any"
 
-# while ! python manage.py flush --no-input 2>&1; do
-#   echo "Flusing django manage command"
-#   sleep 3
-# done
+#while ! python manage.py flush --no-input 2>&1; do
+#  echo "Flusing django manage command"
+#  sleep 3
+#done
+echo "collect static"
+
+python manage.py collectstatic --noinput
 
 echo "Migrate the Database at startup of project"
 
@@ -17,5 +20,24 @@ done
 
 echo "Django docker is fully configured successfully."
 
-exec "$@"
+echo "create table"
 
+while ! python create.py 2>&1; do
+    echo "Creating table"
+    sleep 3
+done
+
+echo "Insert record"
+
+while ! python insert.py 2>&1; do
+    echo "Insert is in progress status"
+    sleep 3
+done
+
+echo "Start server"
+
+python3 manage.py runserver 0.0.0.0:8000
+sleep 3
+
+
+exec "$@"
