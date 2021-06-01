40163db8b0abb0a77e246e9ce834d861dc1cf87d
wolfasterest <sonaster2k@gmail.com>
2021-06-01 13:06:10 +0700

    update blog and post view ver2

--- blogapp/views/post.py
+++ blogapp/views/post.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blogapp.models.Post import Post
-
-
 def post(request, id):
     post_render = Post.objects.get(id= id)
-    return render(request,'blogapp/post.html', {'post_render':post_render})
+    object_list = Post.objects.all()
+    '''if Post.objects.get(id=(id-1))!=None:
+        post_previous =Post.objects.get(id=(id-1))
+    if Post.objects.get(id=(id+1))!=None:
+        post_next =Post.objects.get(id=(id+1))'''
+    return render(request,'blogapp/post.html',
+    {'post_render':post_render,'object_list':object_list})
+    '''post_next':post_next,
+    'post_previous':post_previous'''
+    
