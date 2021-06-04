3cc30a4c936753da2998c11818da757215f83c95
tuannh <tuannh@gmail.com>
2021-06-04 09:06:42 +0700

    setting page

--- blogapp/form.py
+++ blogapp/form.py
@@ -1,13 +1,4 @@
 from django import forms
 
-XSS = (
-    ("1", "Yes"),
-    ("2", "No"),
-)
-CSRF = (
-     ("1", "Yes"),
-    ("2", "No"),
-)
 class Vul(forms.Form):
-    XSS=forms.ChoiceField(choices=XSS)
-    CSRF=forms.ChoiceField(choices=CSRF)
+    
--- blogapp/views/setting.py
+++ blogapp/views/setting.py
@@ -1,25 +1,12 @@
-from blogapp.form import Vul
 from django.http.response import HttpResponse
 from django.shortcuts import render
-import os
-from decouple import config
-
-
-# def setting (request):
-#     VUL = config('VUL')
-#     if request.method=="GET":
-#         VUL = request.GET.get("XSS", "CSRF")
-#         if VUL.is_valid():
-#             return HttpResponse('Da luu')
-#     return render(request, "blogapp/setting.html", {'VUL':VUL})
+from django.contrib import messages
 
 def setting(request):
-    XSS=config('XSS')
-    CSRF=config('CSRF')
-    form = Vul()
+    if request.method == 'GET':
+        return render(request,"blogapp/setting.html",{})
     if request.method == 'POST':
-        form = Vul(request.POST)
-        if form.is_valid():
-            # form.save()
-            return HttpResponse('Luu Thanh Cong')
-    return render(request,"blogapp/setting.html", {'form':form})
+       list = request.POST.getlist('vuls')
+        
+        mess = messages.add_message(request, messages.SUCCESS, 'Form submitted successfully.')
+        return render(request, "blogapp/setting.html", {}) 
