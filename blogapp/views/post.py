a5934862ca19c869733e5dd944f9486ee7b5a241
wolfasterest <sonaster2k@gmail.com>
2021-06-07 11:12:50 +0700

    implement Stored XSS into Comment

--- blogapp/views/post.py
+++ blogapp/views/post.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponseRedirect
+from .form import CommentForm
 from blogapp.models.Post import Post
-
+from blogapp.models.Comment import  Comment
+from blogapp.models.Setting import Vul
 
 def post(request, id):
-    post_render = Post.objects.get(id= id)
     object_list = Post.objects.all()
-    '''if Post.objects.get(id=(id-1))!=None:
-        post_previous =Post.objects.get(id=(id-1))
-    if Post.objects.get(id=(id+1))!=None:
-        post_next =Post.objects.get(id=(id+1))'''
-    return render(request,'blogapp/post.html',
-    {'post_render':post_render,'object_list':object_list})
+    xss = Vul.objects.filter(name="XSS").values()[0]['status']
+    post_render = get_object_or_404(Post, id=id)
+    form = CommentForm()
+    if request.method =="POST":
+        form =CommentForm(data = request.POST,author_id=request.user, post_id=post_render, email=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+    
+    
+    return render(request,'blogapp/post.html',{'object_list':object_list,'post_render':post_render,'form':form,'xss':xss})
     '''post_next':post_next,
     'post_previous':post_previous'''
     
