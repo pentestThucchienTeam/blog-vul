6138ad0eb24d4c9cee4c39b5746ac7999340c7e8
DoTuan1 <thanhtuandohalai@gmail.com>
2021-06-15 07:12:02 +0700

    fix

--- blogapp/views/search.py
+++ blogapp/views/search.py
@@ -30,23 +30,4 @@ def search(request):
     {'query':query, "result":result, "xss": xss, 'sql':sql})
 
     return HttpResponse(html)
-  else:
-    query=""
-    result=[]
-    xss = Vul.objects.filter(name="XSS").values()[0]['status']
-    sql = Vul.objects.filter(name="SQLI").values()[0]['status']
-    if request.method=="GET":
-        query=request.GET.get("search", None)
-        if sql:
-          sqli=   "SELECT * FROM blogapp_post WHERE title ILIKE '%%" + str(query) + "%%'"
-          result=Post.objects.raw(sqli)
-        else:
-          # ORM
-          # result=Post.objects.filter(Q(title__icontains=query)|Q(content__icontains=query))
-          result=Post.objects.raw('SELECT * FROM blogapp_post WHERE title ILIKE  %s',  ['%%' + query + '%%'] )
-
-
-    html = render_to_string('blogapp/search.html',
-    {'query':query, "result":result, "xss": xss, 'sql':sql})
-
-    return HttpResponse(html)
+  
