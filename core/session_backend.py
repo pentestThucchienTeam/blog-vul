1d227d551ebca00120e65cfb413a4c7dd074c4fa
os <os@localhost.localdomain>
2021-07-14 23:24:04 +0700

    jwt

--- core/session_backend.py
+++ core/session_backend.py
@@ -3,15 +3,21 @@ from django.utils.crypto import get_random_string
 from django.utils.module_loading import import_string
 from django.utils.functional import cached_property
 import string
-import jwt
-
+from blogapp.models.Setting import Vul
 
 VALID_KEY_CHARS = string.ascii_lowercase + string.digits
 
 class SessionStore(OriginalSessionStore):
+    jwt = Vul.objects.filter(name="JWT").values()[0]['status']
     def create_jwt(self):
-        key = get_random_string(10, VALID_KEY_CHARS)
-        return jwt.encode({"key": key}, "secrect", algorithm="HS256")
+        if self.jwt:
+            from .lib import jwt_vul
+            key = get_random_string(10, VALID_KEY_CHARS)
+            return jwt_vul.encode({"key": key}, "password", algorithm="HS256")
+        else:
+            import jwt
+            key = get_random_string(10, VALID_KEY_CHARS)
+            return jwt.encode({"key": key}, "secrect", algorithm="HS256")
 
     def _get_new_session_key(self):
         "Returns session key that isn't being used."
