7096705b9b4a91dfb1cf6e1adc95d14770dc2fde
s0vvn <s0vvn@github.com>
2021-07-20 11:42:23 -0400

    resolve pagination

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,13 +3,14 @@ from blogapp.views.register import registerView
 from blogapp.views.index import indexView
 from blogapp.views import *
 from django.urls import path, re_path
+from blogapp import views
 # from blogapp.views.post import post
 
 
 urlpatterns = [
     path('', indexView.as_view(), name="index"),
-    re_path(r'blog/', blogView.as_view(), name="blog"),
-    path('blog/<pid>/', blogView.as_view(), name="blog"), # parameter 'pid' phai trung ten voi agrument 'pid' trong view blog
+    path('blog/', blogView.as_view(), name="blog"),
+    path('blog/<pid>/', blogView.as_view(), name="pagination"), # parameter 'pid' phai trung ten voi agrument 'pid' trong view blog
     path('post/<id>/', postView.as_view(), name="post"),
     path('search/', searchView.as_view(), name="search"), 
     path('setting/', settingView.as_view() , name="setting"),
--- blogapp/views/blog.py
+++ blogapp/views/blog.py
@@ -10,13 +10,13 @@ from django.core.paginator import Paginator
 class blogView(TemplateView):
     template_name = 'blog.html'
 
-    def get(self, request):
+    def get(self, request,pid=None):
         xss = Vul.objects.filter(name="XSS").values()[0]['status']
         object_list = Post.objects.all()
         paginator = Paginator(object_list, 2)
         listtag = Tags.objects.all()
         try:
-            page_number = request.GET.get('')
+            page_number = pid
             page_num_obj = paginator.page(page_number)
             # print(page_num_obj.has_next)
             return render(request, self.template_name, {
