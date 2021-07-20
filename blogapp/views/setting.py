dcde1f5816e7f5b00e5831a90b2b9e203b1e8e43
os <os@localhost.localdomain>
2021-07-20 23:34:28 +0700

    Fix function POST trong setting.py

--- blogapp/views/setting.py
+++ blogapp/views/setting.py
@@ -6,3 +6,16 @@ class settingView(ListView):
 	
 	template_name = 'setting.html'
 	model = Vul
+	nameVuls = Vul.objects.values('name')
+
+	def post(self, request):
+		for _vuls in self.nameVuls:
+			
+			if request.POST[_vuls['name']] == "yes":
+				Vul.objects.filter(name=_vuls['name']).update(status=True)
+			else:
+				Vul.objects.filter(name=_vuls['name']).update(status=False)
+
+		object_list = Vul.objects.all()
+
+		return render(request, self.template_name, {'object_list': object_list})
