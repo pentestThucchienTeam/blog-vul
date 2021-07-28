521490e0bafc418c2d54a20bca18b5a335233ef9
s0vvn <s0vvn@github.com>
2021-07-28 08:39:44 -0400

    fix bug setview

--- blogapp/views/setting.py
+++ blogapp/views/setting.py
@@ -7,13 +7,14 @@ from django.http import HttpResponse , HttpRequest
 from django.template import engines
 from django.contrib.auth import authenticate
 from core.lib import jwt_vul
+from core.lib.jwt_vul.utils import base64url_decode, base64url_encode
 
 
 class settingView(ListView):
 	# ListView mac dinh xu ly request GET nen khong can method GET
 	template_name = 'setting.html'
-	nameVuls = Vul.objects.values()[0]['status']
-	jwt_confusion = Vul.objects.filter(name="JWT_Key_Confusion").values()[0]['status']
+	nameVuls = Vul.objects.values('name')
+	
 	
 	def get(self, request):
 		object_list = Vul.objects.all()
@@ -28,12 +29,13 @@ class settingView(ListView):
 
 	def post(self, request):
 		object_list = Vul.objects.all()
+		jwt_confusion = Vul.objects.filter(name="JWT_Key_Confusion").values()[0]['status']
 		jwts = Vul.objects.filter(name="JWT").values()[0]['status']
 		if request.user.username :
 			cookie_check = request.COOKIES['auth']
-			if self.jwt_confusion and not self.jwts:
+			if jwt_confusion and not jwts:
 				ext, fake = cookie_check.split('.',1)
-				header = base64.urlsafe_b64decode(bytes(str(ext),'utf-8'))
+				header = base64url_decode(bytes(str(ext),'utf-8'))
 				pubkey = json.loads(header.decode('utf-8'))    
 				publickey = pubkey['publickey']
 				cookie_decode = jwt_vul.decode(cookie_check, publickey)
