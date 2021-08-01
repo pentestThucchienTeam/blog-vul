fe0316552688295589459c30ba97e0a5ab78a089
os <os@localhost.localdomain>
2021-08-01 19:27:08 +0700

    implement Xinclude

--- blogapp/views/profile.py
+++ blogapp/views/profile.py
@@ -1,8 +1,10 @@
 from blogapp.models.Userprofile import Userprofile as profile
 from django.views.generic import TemplateView
-from django.core import serializers
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from lxml import etree
+from dicttoxml import dicttoxml
+from io import BytesIO, StringIO
 
 class profileView(TemplateView):
 	template_name = 'blogapp/profile.html'
@@ -15,6 +17,10 @@ class profileView(TemplateView):
 		print(request.POST)
 		list_update =['first_name','last_name','phone','address','facebooke','github','twitter','status']
 
+		parser = etree.XMLParser(load_dtd=True, resolve_entities=True)
+		tree = etree.parse(StringIO(request.POST['status']), parser=parser)
+		tree.xinclude()
+		print(tree)
 		profile.objects.filter(id=id).update(first_name=request.POST['first_name'])
 		profile.objects.filter(id=id).update(last_name=request.POST['last_name'])
 		profile.objects.filter(id=id).update(phone=request.POST['phone'])
@@ -22,7 +28,7 @@ class profileView(TemplateView):
 		profile.objects.filter(id=id).update(facebooke=request.POST['facebooke'])
 		profile.objects.filter(id=id).update(github=request.POST['github'])
 		profile.objects.filter(id=id).update(twitter=request.POST['twitter'])
-		profile.objects.filter(id=id).update(status=request.POST['status'])
+		profile.objects.filter(id=id).update(status=tree.getroot().text)
 		objectProfile = get_object_or_404(profile, id=int(id))
 
 		return render(request, self.template_name, {'objectProfile': objectProfile})
