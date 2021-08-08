31eda834a4c2145a6160b657799830cf772f7b1f
tu4n4nh <tuananh0339@gmail.com>
2021-08-09 00:48:26 +0700

    abc

--- core/settings.py
+++ core/settings.py
@@ -147,5 +147,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-STATICFILES_DIRS = (os.path.join(CORE_DIR, "core/static"),)
+# STATICFILES_DIRS = (os.path.join(CORE_DIR, "core/static"),)
 django_heroku.settings(locals())
