d3d8ecbeae7b65145984921e79e3c213d8d2de5a
Tuấn Anh <tuananh.nta1010@gmail.com>
2021-12-10 11:45:26 +0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,25 @@
+import re
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.views.static import serve
+
+
+def static(prefix, view=serve, **kwargs):
+    """
+    Return a URL pattern for serving files in debug mode.
+    from django.conf import settings
+    from django.conf.urls.static import static
+    urlpatterns = [
+        # ... the rest of your URLconf goes here ...
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    """
+    if not prefix:
+        raise ImproperlyConfigured("Empty static prefix not permitted")
+    return [
+        re_path(r"^%s(?P<path>.*)$" % re.escape(prefix.lstrip("/")), view, kwargs=kwargs),
+    ]
 
 
 urlpatterns = [
@@ -9,5 +27,4 @@ urlpatterns = [
     path("", include("blogapp.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
