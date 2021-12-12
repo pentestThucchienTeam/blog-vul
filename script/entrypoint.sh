7db360bcd4aea87654bbc6c82f375dd6db6a2324
tu4n4nh <tuananh.nta1010@gmail.com>
2021-12-12 15:45:50 +0700

    fix bug show avatar commenter

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,29 +2,29 @@
 echo "start memcached"
 /etc/init.d/memcached restart 2>&1
 
- echo "Makemigrations"
 
- while ! python manage.py makemigrations 2>&1; do
+echo "Makemigrations"
+
+while ! python manage.py makemigrations blogapp 2>&1; do
     echo "Makemigrations"
     sleep 3
- done
+done
 
+echo "Migrate the Database at startup of project"
 
- echo "Migrate the Database at startup of project"
-
-while ! python manage.py migrate  2>&1; do
+while ! python manage.py migrate blogapp 2>&1; do
    echo "Migration is in progress status"
    sleep 3
 done
 
- echo "Django docker is fully configured successfully."
+echo "Django docker is fully configured successfully."
 
- echo "Insert record"
+echo "Insert record"
 
- while ! python script/insert.py 2>&1; do
+while ! python script/insert.py 2>&1; do
      echo "Insert is in progress status"
      sleep 3
- done
+done
 
 echo "Start server"
 
